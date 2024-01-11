5fea2f85fc46febf163388e7aa3ee3c24f34a05f
dacorm <stolen.youth@yandex.ru>
2024-01-11 12:50:38 +0700

    fix: styles

--- src/pages/auth/sign-in/model.ts
+++ src/pages/auth/sign-in/model.ts
@@ -1,1 +1,8 @@
+import { createEvent, createStore } from "effector";
+
 export type SignInError = "UnknownError" | "InvalidEmail" | "RateLimit";
+
+const emailChanged = createEvent<string>();
+const $email = createStore("");
+
+$email.on(emailChanged, (_, email) => email);
